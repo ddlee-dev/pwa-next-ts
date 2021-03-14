a0370f93aea4c04f45125dcf84d855f35bc08239
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-03-14 11:47:42 -0500

    update gitignore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,5 +7,7 @@ const store = configureStore({
   }
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
