bf7b660a912cc5947d9819ac4b0cbd268e704590
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-03-31 07:26:46 -0500

    update props

--- src/client/components/Button/Button.tsx
+++ src/client/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 interface ButtonProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const ButtonComponent = ({ children, ...props }: ButtonProps) => {
-  return <button>{children}</button>;
+const ButtonComponent = ({ children, className = '', ...props }: ButtonProps) => {
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default ButtonComponent;
