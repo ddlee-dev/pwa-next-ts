087e3995cde0c066154b6b491ebc357dde463f21
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-01 12:56:32 -0500

    add github actions and asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withPWA = require('next-pwa');
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = withPWA({
-  basePath: '/pwa-next-ts',
+  assetPrefix: 'https://inquisitive-entropy.github.io/pwa-next-ts/',
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
