6b53518ec2965eb9e9a006f44ee8fc1f6d77a29f
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-01 14:03:43 -0500

    replace yarn with npm and update actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withPWA = require('next-pwa');
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+const assetPrefix = ghPages ? 'https://inquisitive-entropy.github.io/pwa-next-ts/' : '';
 
 module.exports = withPWA({
-  assetPrefix: 'https://inquisitive-entropy.github.io/pwa-next-ts/',
+  assetPrefix: assetPrefix,
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
