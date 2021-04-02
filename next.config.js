2e2f0bfa27fedc05d257cfb26a315436b761f0e2
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 09:59:47 -0500

    update asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require('next-pwa');
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+const assetPrefix = ghPages ? '/pwa-next-ts/' : '';
 
 module.exports = withPWA({
+  assetPrefix: assetPrefix,
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
