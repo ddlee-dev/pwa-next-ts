5eb540dfae1bca5e2358d2d7bd2e43868931d1aa
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 09:07:18 -0500

    add more actions config

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,13 +3,17 @@ module.exports = {
     fastRefresh: true
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-postcss'
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
-      test: /\.scss$/,
+      test: /\.s?css$/i,
       use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
     });
-
     return config;
   }
 };
