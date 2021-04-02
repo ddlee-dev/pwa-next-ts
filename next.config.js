c837cd7ea328c5c0ced5860ebbe2fe46ed051208
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 09:36:16 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withPWA = require('next-pwa');
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-const assetPrefix = ghPages ? 'https://inquisitive-entropy.github.io/pwa-next-ts/' : '';
 
 module.exports = withPWA({
-  assetPrefix: assetPrefix,
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
