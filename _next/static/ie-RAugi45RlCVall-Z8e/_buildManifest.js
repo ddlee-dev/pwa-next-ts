61fe95c321374ad53d4360b64c175fa511eca01f
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 15:52:18 +0000

    Deploying to gh-pages from @ inquisitive-entropy/pwa-next-ts@33b1cd4d94bbfc11e2732ba42d9535fb5446f335 🚀

---
+++ _next/static/ie-RAugi45RlCVall-Z8e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-f63d86b802bb46ba8a7a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f54bd0c3f10f89ba1510.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
