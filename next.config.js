0dad8bee1fe78aef49d5e2fb964ae91f503e8c3e
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 12:01:22 -0500

    update pwa path

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
-    register: true
+    register: true,
+    subdomainPrefix: basePath
   }
 });
