62b2a80a9131aafd51367938ef779b11e365c32e
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-04-02 11:08:00 -0500

    fix basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withPWA = require('next-pwa');
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-const ghPrefix = ghPages ? '/pwa-next-ts/' : '';
+const basePath = ghPages ? '/pwa-next-ts' : '';
+const assetPrefix = ghPages ? '/pwa-next-ts/' : '';
 
 module.exports = withPWA({
-  basePath: ghPrefix,
-  assetPrefix: ghPrefix,
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
